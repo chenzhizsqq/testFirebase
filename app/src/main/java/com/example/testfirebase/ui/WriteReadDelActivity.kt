9881335ec9f2f62cc9353c38b4e7c2379f200ec2
chenzhi <chenzhi@qq.com>
2022-01-19 16:38:59 +0900

    /json写入和读取

--- app/src/main/java/com/example/testfirebase/ui/WriteReadDelActivity.kt
+++ app/src/main/java/com/example/testfirebase/ui/WriteReadDelActivity.kt
@@ -1,10 +1,14 @@
 package com.example.testfirebase.ui
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 import com.example.testfirebase.databinding.ActivityWriteReadDelBinding
 import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
 
 //Firebase Realtime Database 是一种托管在云端的数据库。
@@ -14,6 +18,7 @@ import com.google.firebase.ktx.Firebase
 //https://qiita.com/YoshitakaOkada/items/ebbb83c199750742d1d2
 class WriteReadDelActivity : AppCompatActivity() {
     private lateinit var binding: ActivityWriteReadDelBinding
+    private val testJsonDir = "testJsonDir"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityWriteReadDelBinding.inflate(layoutInflater)
@@ -58,5 +63,59 @@ class WriteReadDelActivity : AppCompatActivity() {
                 Toast.makeText(this, "不成功删除", Toast.LENGTH_SHORT).show()
             }
         }
+
+        //json写入
+        jsonWrite()
+
+        //json读取
+        jsonRead()
     }
+
+    val mTestReadViewModel = TestViewModel()
+    private fun jsonRead() {
+        binding.jsonRead.setOnClickListener {
+            val database = Firebase.database
+            database.getReference(testJsonDir).get().addOnSuccessListener {
+                Log.e("TAG", "jsonRead:" + it.value)
+                Toast.makeText(this, "${it.value}" , Toast.LENGTH_SHORT).show()
+
+                mTestReadViewModel.postsDataList.postValue(it.value as List<PostsData>)
+            }
+        }
+        //观察是否正在读取数据，做出不同的操作
+        mTestReadViewModel.postsDataList.observe(this, {
+            Log.e("TAG", "jsonRead:mTestReadViewModel.postsDataList: $it")
+        })
+    }
+
+
+    //json写入 jsonWrite() begin
+    lateinit var mJsonWriteList : ArrayList<PostsData>
+    private fun jsonWrite() {
+        val mTestViewModel = TestViewModel()
+        binding.jsonWrite.setOnClickListener {
+            mJsonWriteList = ArrayList()
+            for (i in 0..5) {
+                val mPostsData = PostsData(i, "${binding.etWrite.text}$i")
+                mJsonWriteList.add(mPostsData)
+            }
+            mTestViewModel.postsDataList.postValue(mJsonWriteList)
+        }
+        //观察是否正在读取数据，做出不同的操作
+        mTestViewModel.postsDataList.observe(this, {
+            val database = Firebase.database
+            database.getReference(testJsonDir).setValue(it).addOnSuccessListener {
+                Toast.makeText(this, "json写入!", Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+    //json写入 jsonWrite() end
+
+
+
+}
+data class PostsData(val id: Int, val title: String)
+class TestViewModel : ViewModel() {
+    //专门对应json数据中的posts数据List
+    val postsDataList = MutableLiveData<List<PostsData>>()
 }
