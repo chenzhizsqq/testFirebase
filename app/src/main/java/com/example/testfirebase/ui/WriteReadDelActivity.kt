1f3251ccea6f688e23adb6f8256378fa72662141
chenzhi <chenzhi@qq.com>
2022-01-19 09:58:09 +0900

    添加说明

--- app/src/main/java/com/example/testfirebase/ui/WriteReadDelActivity.kt
+++ app/src/main/java/com/example/testfirebase/ui/WriteReadDelActivity.kt
@@ -7,6 +7,10 @@ import com.example.testfirebase.databinding.ActivityWriteReadDelBinding
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
+//Firebase Realtime Database 是一种托管在云端的数据库。
+// 数据以 JSON 格式存储并实时同步到每个连接的客户端。
+// 当您使用我们的 Apple 平台、Android 和 JavaScript SDK 构建跨平台应用时，
+// 所有的客户端共享一个 Realtime Database 实例，并自动接收包含最新数据的更新。
 //https://qiita.com/YoshitakaOkada/items/ebbb83c199750742d1d2
 class WriteReadDelActivity : AppCompatActivity() {
     private lateinit var binding: ActivityWriteReadDelBinding
