485a558d738733456d0050b20eb893887601e9fb
chenzhi <chenzhi@qq.com>
2022-01-25 10:38:26 +0900

    Cloud Storage：共享用户生成的内容（如图片和视频） 开始使用 https://firebase.google.com/docs/storage/android/start

--- app/src/main/java/com/example/testfirebase/ui/CloudStorageActivity.kt
+++ app/src/main/java/com/example/testfirebase/ui/CloudStorageActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.testfirebase.databinding.ActivityCloudStorageBinding
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.ktx.initialize
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.ktx.storage
 
@@ -24,5 +25,27 @@ class CloudStorageActivity : AppCompatActivity() {
         storage = Firebase.storage
         // [END storage_field_initialization]
 
+    }
+
+
+    // 使用多个 Cloud Storage 存储桶：这个不是默认的云空间，是其他的云空间地址
+    fun nonDefaultBucket() {
+        // [START storage_non_default_bucket]
+        // Get a non-default Storage bucket
+        val storage = Firebase.storage("gs://my-custom-bucket")
+        // [END storage_non_default_bucket]
+    }
+
+    //使用自定义 Firebase 应用 : 如果您要使用自定义 FirebaseApp 构建更复杂的应用，可以创建使用该应用初始化的
+    fun customApp() {
+        val customApp = Firebase.initialize(this)
+
+        // [START storage_custom_app]
+        // Get the default bucket from a custom FirebaseApp
+        val storage = Firebase.storage(customApp!!)
+
+        // Get a non-default bucket from a custom FirebaseApp
+        val customStorage = Firebase.storage(customApp, "gs://my-custom-bucket")
+        // [END storage_custom_app]
     }
 }
