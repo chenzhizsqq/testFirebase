477325ecf4b93f344bdd12de9993d0d5eddb4213
chenzhi <chenzhi@qq.com>
2022-01-25 10:21:44 +0900

    Cloud Storage：共享用户生成的内容（如图片和视频） storage: FirebaseStorage 的创建

--- app/src/main/java/com/example/testfirebase/ui/CloudStorageActivity.kt
+++ app/src/main/java/com/example/testfirebase/ui/CloudStorageActivity.kt
@@ -3,13 +3,26 @@ package com.example.testfirebase.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.testfirebase.databinding.ActivityCloudStorageBinding
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.ktx.storage
+
 
 class CloudStorageActivity : AppCompatActivity() {
     private lateinit var binding: ActivityCloudStorageBinding
+
+    // [START storage_field_declaration]
+    lateinit var storage: FirebaseStorage
+    // [END storage_field_declaration]
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCloudStorageBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // [START storage_field_initialization]
+        storage = Firebase.storage
+        // [END storage_field_initialization]
+
     }
 }
