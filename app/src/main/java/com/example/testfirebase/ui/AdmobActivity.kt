4f1d0d19475d27d5fac797167eac5897cb07db85
chenzhi <chenzhi@qq.com>
2022-01-27 18:04:30 +0900

    admob : 广告设置 简单的广告显示：暂时还没有成功 测试用的广告显示：已经成功

--- app/src/main/java/com/example/testfirebase/ui/AdmobActivity.kt
+++ app/src/main/java/com/example/testfirebase/ui/AdmobActivity.kt
@@ -1,10 +1,14 @@
 package com.example.testfirebase.ui
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.testfirebase.databinding.ActivityAdmobBinding
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.RequestConfiguration
+import java.util.*
 
 
 class AdmobActivity : AppCompatActivity() {
@@ -16,6 +20,13 @@ class AdmobActivity : AppCompatActivity() {
         binding = ActivityAdmobBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.simpleCreate.setOnClickListener { simpleCreate() }
+        binding.setTestCreate.setOnClickListener { setTestCreate() }
+
+    }
+
+    // 简单的广告显示
+    private fun simpleCreate() {
         // Initialize the Mobile Ads SDK with an AdMob App ID.
         MobileAds.initialize(this) {}
 
@@ -25,9 +36,31 @@ class AdmobActivity : AppCompatActivity() {
         // Start loading the ad in the background.
         binding.adView.loadAd(adRequest)
 
+        logToast("简单的广告显示")
+    }
 
-        // 自己测试用的
+    // 创建测试用的广告显示
+    //https://developers.google.com/admob/android/test-ads
+    private fun setTestCreate() {
+        val testDeviceIds = Arrays.asList("E9CCC26CCC75A7E43D70B3C4E286EBF0")
+        val configuration = RequestConfiguration.Builder().setTestDeviceIds(testDeviceIds).build()
+        MobileAds.setRequestConfiguration(configuration)
+
         val adRequest2 = AdRequest.Builder().build()
-        binding.adView2.loadAd(adRequest2)
+        if (adRequest2.isTestDevice(this)) {
+            logToast("测试用的广告显示")
+        }
+
+        binding.adViewTest.loadAd(adRequest2)
+    }
+
+    private fun logToast(str: String) {
+        Toast.makeText(baseContext, str, Toast.LENGTH_SHORT).show()
+        Log.e(TAG, str)
+    }
+
+    companion object {
+
+        private const val TAG = "AdmobActivity"
     }
 }
