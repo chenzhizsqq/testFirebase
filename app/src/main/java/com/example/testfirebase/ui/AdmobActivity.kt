83b929dba1ce052c2b46dd8d8537d508b4df0a64
chenzhi <chenzhi@qq.com>
2022-01-28 10:35:22 +0900

    admob : 广告设置 插页式广告

--- app/src/main/java/com/example/testfirebase/ui/AdmobActivity.kt
+++ app/src/main/java/com/example/testfirebase/ui/AdmobActivity.kt
@@ -5,15 +5,18 @@ import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.testfirebase.databinding.ActivityAdmobBinding
-import com.google.android.gms.ads.AdRequest
-import com.google.android.gms.ads.MobileAds
-import com.google.android.gms.ads.RequestConfiguration
+import com.google.android.gms.ads.*
+import com.google.android.gms.ads.interstitial.InterstitialAd
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
 import java.util.*
 
 
 class AdmobActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityAdmobBinding
+
+    //插入式广告
+    private var mInterstitialAd: InterstitialAd? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,7 +25,56 @@ class AdmobActivity : AppCompatActivity() {
 
         binding.simpleCreate.setOnClickListener { simpleCreate() }
         binding.setTestCreate.setOnClickListener { setTestCreate() }
+        binding.setInterstitialAd.setOnClickListener { setInterstitialAd() }
 
+    }
+
+    //https://developers.google.com/admob/android/interstitial
+    //测试账号的插页式广告
+    private fun setInterstitialAd() {
+
+        val adRequest = AdRequest.Builder().build()
+
+        //加载广告
+        InterstitialAd.load(this,
+            "ca-app-pub-3940256099942544/1033173712",
+            adRequest,
+            object : InterstitialAdLoadCallback() {
+                override fun onAdFailedToLoad(adError: LoadAdError) {
+                    logToast(adError.message)
+                    mInterstitialAd = null
+                }
+
+                override fun onAdLoaded(interstitialAd: InterstitialAd) {
+                    logToast("广告开始读取")
+                    mInterstitialAd = interstitialAd
+
+                    //FullScreenContentCallback 处理与展示 InterstitialAd 相关的事件。
+                    // 在展示 InterstitialAd 之前，请务必按如下方式设置回调：
+                    mInterstitialAd?.fullScreenContentCallback =
+                        object : FullScreenContentCallback() {
+                            override fun onAdDismissedFullScreenContent() {
+                                logToast("Ad was dismissed.")
+                            }
+
+                            override fun onAdFailedToShowFullScreenContent(adError: AdError?) {
+                                logToast("Ad failed to show.")
+                            }
+
+                            override fun onAdShowedFullScreenContent() {
+                                logToast("广告正在全屏展示")
+                                mInterstitialAd = null
+                            }
+                        }
+
+                    //展示广告
+                    if (mInterstitialAd != null) {
+                        mInterstitialAd?.show(this@AdmobActivity)
+                    } else {
+                        logToast("The interstitial ad wasn't ready yet.")
+                    }
+                }
+            })
     }
 
     // 简单的广告显示
